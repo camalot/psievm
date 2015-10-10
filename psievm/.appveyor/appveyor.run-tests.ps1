206db4fa06aaa57be8f6124342a94ae21706139a
Ryan Conrad <camalot@gmail.com>
2015-10-10 15:21:43 -0500

    Test chocolateyPath

--- psievm/.appveyor/appveyor.run-tests.ps1
+++ psievm/.appveyor/appveyor.run-tests.ps1
@@ -1,14 +1,21 @@
+if(-not $env:ChocolateyInstall) {
+	throw "Unable to use chocolatey install path";
+}
 
-$pesterBin = (Join-Path -Path $env:ChoclateyInstall -ChildPath "\lib\pester\tools\bin\pester.bat");
+$pesterBin = (Join-Path -Path $env:ChocolateyInstall -ChildPath "\lib\pester\tools\bin\pester.bat");
 
 if(!(Test-Path -Path $pesterBin)) {
 	choco install pester -y -force | Write-Host;
+
+	if(!(Test-Path -Path $pesterBin)) {
+		throw "Pester not installed."
+	}
 }
 
 
 $currentDirectory = $PWD;
 
-Set-Location -Path "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm.tests" | Out-Null;
+Set-Location -Path (Join-Path -Path "$env:APPVEYOR_BUILD_FOLDER" -ChildPath "psievm\psievm.tests\") | Out-Null;
 
 Copy-Item -Path "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm\psievm.ps*1" -Destination "$PWD" -Force | Write-Host;
 
