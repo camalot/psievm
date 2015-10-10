0951716263a85474168c0e4fde2ed3d9eb728118
Ryan Conrad <camalot@gmail.com>
2015-10-10 15:48:06 -0500

    Test for the module, not the batch. added coverage (i think this is the right way)

--- psievm/.appveyor/appveyor.run-tests.ps1
+++ psievm/.appveyor/appveyor.run-tests.ps1
@@ -1,29 +1,17 @@
-if(-not $env:ChocolateyInstall) {
-	throw "Unable to use chocolatey install path";
-}
 
-$pesterBin = (Join-Path -Path $env:ChocolateyInstall -ChildPath "\lib\pester\tools\bin\pester.bat");
-
-if(!(Test-Path -Path $pesterBin)) {
-	choco install pester -y -force | Write-Host;
-
-	if(!(Test-Path -Path $pesterBin)) {
-		throw "Pester not installed."
-	}
+if(-not (Get-Module -ListAvailable -Name "pester")) {
+	choco install pester -y | Write-Host;
 }
 Import-Module "pester" -Verbose -Force;
-
-
 $currentDirectory = $PWD;
 $workingDir = (Join-Path -Path "$env:APPVEYOR_BUILD_FOLDER" -ChildPath "psievm\psievm.tests\");
 "Moving to '$workingDir'" | Write-Host;
 Set-Location -Path $workingDir | Out-Null;
+$coverageFiles = "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm\psievm.ps*1";
 
-Copy-Item -Path "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm\psievm.ps*1" -Destination "$PWD" -Force | Write-Host;
-
-#(& cmd /c `"$pesterBin`" *>&1);
+Copy-Item -Path "$coverageFiles" -Destination "$PWD" -Force | Write-Host;
 $tests = (Get-ChildItem -Path "$workingDir\*.Tests.ps1" | % { $_.FullName });
-Invoke-Pester -Script $tests;
+Invoke-Pester -Script $tests -CodeCoverage $coverageFiles;
 
 "Moving back to '$currentDirectory'" | Write-Host;
 Set-Location $currentDirectory | Out-Null;
