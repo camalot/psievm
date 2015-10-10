4b731e5ee6d9cd49298aa8111497a58c58a9608e
Ryan Conrad <camalot@gmail.com>
2015-10-10 15:38:11 -0500

    set up pester invoke

--- psievm/.appveyor/appveyor.run-tests.ps1
+++ psievm/.appveyor/appveyor.run-tests.ps1
@@ -22,7 +22,7 @@ Set-Location -Path $workingDir | Out-Null;
 Copy-Item -Path "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm\psievm.ps*1" -Destination "$PWD" -Force | Write-Host;
 
 #(& cmd /c `"$pesterBin`" *>&1);
-$tests = Get-ChildItem -Path "$workingDir\**\*.Tests.ps1";
+$tests = (Get-ChildItem -Path "$workingDir\*.Tests.ps1" | select -ExpandProperty name) -join $workingDir;
 Invoke-Pester -Script $tests -CodeCoverage $tests;
 
 "Moving back to '$currentDirectory'" | Write-Host;
