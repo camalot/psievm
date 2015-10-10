762f6ad55b2fb03c741f2c86de4a20149b0f6e19
Ryan Conrad <camalot@gmail.com>
2015-10-09 23:15:57 -0500

    try to install ps4

--- psievm/.appveyor/appveyor.install.ps1
+++ psievm/.appveyor/appveyor.install.ps1
@@ -1,4 +1,4 @@
-#choco install powershell4 -y;
+choco install powershell4 -y;
 #choco install powershell -pre -y;
 #Write-Host "Loading Choco Log";
 #(Get-Content -Path "C:\ProgramData\chocolatey\logs\chocolatey.log" -Raw) | Write-Host;
