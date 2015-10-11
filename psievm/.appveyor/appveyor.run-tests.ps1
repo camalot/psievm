75205b4e0687da70568d2bdbffe210d1f6b19816
Ryan Conrad <camalot@gmail.com>
2015-10-10 23:58:20 -0500

    changed dir to run tests

--- psievm/.appveyor/appveyor.run-tests.ps1
+++ psievm/.appveyor/appveyor.run-tests.ps1
@@ -4,8 +4,9 @@ if(-not (Get-Module -ListAvailable -Name "pester")) {
 }
 
 Import-Module "pester" -Verbose -Force;
+$cdir = $PWD;
 $workingDir = (Join-Path -Path "$env:APPVEYOR_BUILD_FOLDER" -ChildPath "psievm\psievm.tests\");
-
+Set-Location -Path $workingDir | Out-Null;
 $psModuleFiles = "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm\psievm.ps*1";
 $psChocoFiles = "$env:APPVEYOR_BUILD_FOLDER\psievm\psievm.package\tools\*.ps1";
 Copy-Item -Path "$psModuleFiles" -Destination "$workingDir" -Force -Verbose;
@@ -13,3 +14,4 @@ Copy-Item -Path "$psChocoFiles" -Destination "$workingDir" -Force -Verbose;
 $tests = (Get-ChildItem -Path "$workingDir\*.Tests.ps1" | % { $_.FullName });
 Invoke-Pester -Script $tests;
 
+Set-Location -Path $cdir | Out-Null;
