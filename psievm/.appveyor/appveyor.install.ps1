9d30721fad1d31ddf0aa7e87d34c51a95f5a52f8
Ryan Conrad <camalot@gmail.com>
2015-10-20 18:11:43 -0500

    testing the installer again, and remote to see what is happening.

--- psievm/.appveyor/appveyor.install.ps1
+++ psievm/.appveyor/appveyor.install.ps1
@@ -1,16 +1,16 @@
-choco install powershell4 -y;
+choco install powershell -y -version 4.0.20141001;
 
-#$oneget = (Join-Path -Path $env:APPVEYOR_BUILD_FOLDER -ChildPath "oneget");
-#if(!(Test-Path -Path $oneget)) {
-#	New-Item -Path $oneget -ItemType Directory | Out-Null;
-#}
-#$pmn = (Join-Path -Path $oneget -ChildPath "PackageManagement.0.1.0.29315.nupkg");
-#$webclient = (New-Object system.net.webclient);
-#$webclient.DownloadFile($env:PackageManagementPackageUrl, $pmn);
+$oneget = (Join-Path -Path $env:APPVEYOR_BUILD_FOLDER -ChildPath "oneget");
+if(!(Test-Path -Path $oneget)) {
+	New-Item -Path $oneget -ItemType Directory | Out-Null;
+}
+$pmn = (Join-Path -Path $oneget -ChildPath "PackageManagement.0.1.0.29315.nupkg");
+$webclient = (New-Object system.net.webclient);
+$webclient.DownloadFile($env:PackageManagementPackageUrl, $pmn);
 
-#choco install PackageManagement -y -source $oneget
+choco install PackageManagement -y -source $oneget
 
 # if this fails, then the install above failed.
 
-#Get-PackageProvider -Name NuGet -ForceBootstrap;
-#Install-Module -Name PSScriptAnalyzer -Force;
+Get-PackageProvider -Name NuGet -ForceBootstrap;
+Install-Module -Name PSScriptAnalyzer -Force;
