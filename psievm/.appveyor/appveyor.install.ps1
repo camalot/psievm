dd48227ae8ec29fa5002aa2320e0f709ba4a5f35
Ryan Conrad <camalot@gmail.com>
2015-10-26 20:51:18 -0500

    updated the script with logging as it didnt seem to work.

--- psievm/.appveyor/appveyor.install.ps1
+++ psievm/.appveyor/appveyor.install.ps1
@@ -1,9 +1,11 @@
 choco install powershell -y -version 4.0.20141001;
 
-$msiPath = "$($env:USERPROFILE)\PackageManagement_x64.msi"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x64.msi', $msiPath)
-cmd /c start /wait msiexec /i $msiPath /quiet
-
+$msiPath = "$($env:USERPROFILE)\PackageManagement_x64.msi";
+Write-Host "Downloading PackageManagement_x64.msi";
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x64.msi', $msiPath);
+Write-Host "Installing PackageManagement_x64.msi";
+cmd /c start /wait msiexec /i $msiPath /quiet;
+Write-Host "PackageManagement_x64.msi Installed";
 
 ## if this fails, then the install above failed.
 
